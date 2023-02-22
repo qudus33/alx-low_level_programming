25397f45a0ebfa000375216567632a36b5dc800c
Abdulqudus Suleiman <qudus1233@gmail.com>
2023-02-22 19:58:18 +0100

    update

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * times_table - print times table of a number
+ */
+void times_table(n)
+{
+  int i, j, k, n;
+  for 
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,5 +15,6 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
