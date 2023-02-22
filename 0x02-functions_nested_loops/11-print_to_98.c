293ba601002357ce50729b9acddccc7b604e595d
Abdulqudus Suleiman <qudus1233@gmail.com>
2023-02-22 03:20:43 +0100

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,16 +4,22 @@
  * print_to_98 - program to print natural numbers from n to 98
  * @n: function args
  */
-void print_to_98(int n) {
-    int i;
-    if (n <= 98) {
-        for (i = n; i < 98; i++) {
-            printf("%d, ", i);
-        }
-    } else {
-        for (i = n; i > 98; i--) {
-            printf("%d, ", i);
-        }
-    }
-    printf("98\n");
+void print_to_98(int n)
+{
+int i;
+if (n <= 98)
+{
+for (i = n; i < 98; i++)
+{
+printf("%d, ", i);
+}
+}
+else
+{
+for (i = n; i > 98; i--)
+{
+printf("%d, ", i);
+}
+}
+printf("98\n");
 }
