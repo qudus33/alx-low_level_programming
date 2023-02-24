a71d414c773baf6c5b15837edd6b04505139f68c
Abdulqudus Suleiman <qudus1233@gmail.com>
2023-02-25 00:02:40 +0100

    print integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,21 +6,14 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
-
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+    
+    if (n / 10 != 0) {
+        print_number(n / 10);
+    }
+    
+    _putchar(n % 10 + '0');
 }
