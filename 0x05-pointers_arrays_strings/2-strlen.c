47b24ac16eccdd589e89369b4bcc7dc19f5f467d
Abdulqudus Suleiman <qudus1233@gmail.com>
2023-02-27 22:40:29 +0100

    lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - print the length of a string
+ *
+ * @s: argument s
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
