e72e4a7a8578347714733ede41ea7b241740032b
Abdulqudus Suleiman <qudus1233@gmail.com>
2023-02-28 03:43:27 +0100

    half string update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,21 @@ void puts_half(char *str)
 	int length = strlen(str);
 	int half_length = length / 2;
 
-	for (i = half_length; i < length; i++)
+	if (length % 2 == 0)
 	{
-		_putchar(str[i]);
+		for (i = half_length; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		int new_half_length = (length + 1) / 2;
+
+		for (i = new_half_length; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
