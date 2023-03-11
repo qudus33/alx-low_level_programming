76a6117ae755e2dfb6457a7d95bebea1634aa55a
Abdulqudus Suleiman <qudus1233@gmail.com>
2023-03-11 17:41:43 +0100

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print name
+ * @argc: count the argument passed
+ * @argv: gets the argument passed
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
