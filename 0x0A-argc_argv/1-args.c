6a35a2be3f05dbbc1d3f2b87fb6e3e075005ab1c
Abdulqudus Suleiman <qudus1233@gmail.com>
2023-03-11 17:52:01 +0100

    c arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,12 @@
 
 /**
  * main - print number of arguments
- * @argc: gets the argument passed
+ * @argc: number of argument passed
+ * @argv: gets the argument passed
  * Return: 0 on success
  */
 
-int main(int argc)
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
